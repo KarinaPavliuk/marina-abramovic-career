a701ea4cb8ccc4789742c675c7071c710671ffd5
Karina Pavliuk <kpavliuk8@gmail.com>
2024-02-19 01:27:59 +0200

    banner markup

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,18 +2,27 @@ import css from './Banner.module.css';
 
 const Banner = () => {
   return (
-    <div className={css.bannerContainer}>
-      <h1 className={css.bannerHeading}>
+    <div className={css.container}>
+      <h1 className={css.heading}>
         <span>Marina</span>
         <span>Abramović</span>
-        <span>Biography</span>
+        <span className={css.headinLastWord}>Biography</span>
       </h1>
-      <p className={css.bannerDescription}></p>
-      <img className={css.bannerImage} src="#" alt="Marina Abramović"></img>
-      <img className={css.bannerImage} src="#" alt="Marina Abramović"></img>
+      <p className={css.bannerDescription}>
+        Serbian conceptual and performance artist.
+      </p>
+      <img
+        className={css.bannerImage}
+        src={'../../images/banner_1.png'}
+        alt="Marina Abramović"
+      ></img>
+      <img
+        className={css.bannerImage}
+        src="../../images/banner_2.png"
+        alt="Marina Abramović"
+      ></img>
     </div>
-  )
-}
+  );
+};
 
 export default Banner;
-
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,23 @@ import css from './Header.module.css';
 const Header = () => {
   return (
     <header className={css.headerContainer}>
-      <div className={css.logo}>LOGO</div>
+      <div className={css.logo}>
+        <a href="#">LOGO</a>
+      </div>
       <nav>
         <ul>
-          <li>ABOUT</li>
-          <li>CAREER</li>
-          <li>AWARDS</li>
-          <li>CONTACTS</li>
+          <li>
+            <a href="#">ABOUT</a>
+          </li>
+          <li>
+            <a href="#">CAREER</a>
+          </li>
+          <li>
+            <a href="#">AWARDS</a>
+          </li>
+          <li>
+            <a href="#">CONTACTS</a>
+          </li>
         </ul>
       </nav>
     </header>
