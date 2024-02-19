94fa52886d336ebe5c3be1139ec332d4cd450c22
Karina Pavliuk <kpavliuk8@gmail.com>
2024-02-19 23:13:00 +0200

    update banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,24 +5,24 @@ import secondImage from '../../images/banner_second_2x.png';
 const Banner = () => {
   return (
     <div className={css.container}>
-      <img
-        className={css.firstImage}
-        src={firstImage}
-        alt="Marina Abramović"
-      ></img>
       <h1 className={css.heading}>
-        <span>Marina</span>
-        <span>Abramović</span>
-        <span className={css.headinLastWord}>Biography</span>
+        <img
+          className={css.firstImage}
+          src={firstImage}
+          alt="Marina Abramović"
+        ></img>
+        <span className={css.headingFirstWord}>Marina</span>
+        <span className={css.headingSecondWord}>Abramović</span>
+        <span className={css.headingLastWord}>Biography</span>
+        <img
+          className={css.secondImage}
+          src={secondImage}
+          alt="Marina Abramović"
+        ></img>
       </h1>
-      <p className={css.bannerDescription}>
+      <p className={css.description}>
         Serbian conceptual and performance artist.
       </p>
-      <img
-        className={css.secondImage}
-        src={secondImage}
-        alt="Marina Abramović"
-      ></img>
     </div>
   );
 };
